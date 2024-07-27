63a7521324ccfcbb39e3dba94ea127fd37d55a39
ayushi7296 <ayushidwivedi7296@gmail.com>
2024-07-27 21:20:09 +0530

    ownership concept

---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/home/ayushi/Rust/ownership/target/debug/ownership: /home/ayushi/Rust/ownership/src/main.rs
---
+++ ownership/target/rls/debug/deps/ownership-921b1613fcff71b1.d
@@ -0,0 +1,5 @@
+/home/ayushi/Rust/ownership/target/rls/debug/deps/ownership-921b1613fcff71b1.rmeta: src/main.rs
+
+/home/ayushi/Rust/ownership/target/rls/debug/deps/ownership-921b1613fcff71b1.d: src/main.rs
+
+src/main.rs:
